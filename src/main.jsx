02dbdce7b2569bf46232796b3f4f8089eb01a086
rafaelanobre <raafanobre@gmail.com>
2023-05-27 16:18:51 -0300

    :wrench: Import axios

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import axios from 'axios'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    axios.defaults.headers.common['Authorization'] = 'Da3XNfDHx4X7NJWZEq9v7eO1';
     <App />
   </React.StrictMode>,
 )
